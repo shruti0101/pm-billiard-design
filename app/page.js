beb8d1477b878a64b1846e0cbb731e50ebc15ff8
shruti0101 <shrutiguptacu@gmail.com>
2026-02-19 11:19:45 +0530

    error check

--- app/page.js
+++ app/page.js
@@ -1,23 +1,22 @@
 import Hero from "@/components/Landingpage/Hero";
 import Whychoose from "@/components/Landingpage/Whychoose";
-import Clientele from "@/components/Landingpage/Clientele"
+
 import AboutUs from "@/components/Landingpage/About";
 import Products from "@/components/Landingpage/Products";
-import ProductCategorySection from "@/components/Landingpage/Categories";
+// import ProductCategorySection from "@/components/Landingpage/Categories";
 import Cta from "@/components/Landingpage/Cta";
-import Popup from "@/components/Popup"
+// import Popup from "@/components/Popup"
 import Enquiry from "@/components/Enquiry"
-import Gallery from "@/components/Gallery";
-import YoutubeSection from "@/components/Landingpage/Youtube";
-import Testimonial from "@/components/Landingpage/Testmonial";
+
+
 import Letter from "@/components/Landingpage/Letter";
 import PremiumTablesSection from "@/components/Landingpage/Premium";
-import Cta2 from "@/components/Landingpage/Cta2";
+
 import Soccer from "@/components/Landingpage/Soccer";
 
 import Faq from "@/components/Landingpage/Faq";
 
-import Formsection from "@/components/Landingpage/Formsection";
+
 
 
 
@@ -26,9 +25,9 @@ export default function Home() {
 <>
 
 
-<Popup></Popup>
+{/* <Popup></Popup> */}
 <Hero></Hero> 
-<ProductCategorySection></ProductCategorySection>
+{/* <ProductCategorySection></ProductCategorySection> */}
 
 <Enquiry></Enquiry>
 <AboutUs></AboutUs>

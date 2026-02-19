e264d86d1d43a1cb4f9f76de92563f448ee88084
shruti0101 <shrutiguptacu@gmail.com>
2026-02-19 17:29:46 +0530

    imge add

--- components/Landingpage/Navbar.jsx
+++ components/Landingpage/Navbar.jsx
@@ -242,13 +242,7 @@ export default function Navbar() {
           document.body,
         )}
 
-      <a
-        href="https://wa.me/9999402424"
-        target="_blank"
-        className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-xl"
-      >
-        <FaWhatsapp size={22} />
-      </a>
+   
     </>
   );
 }
